31fd0a44e9e8dc3042de8921f687da2fa5db8aff
MosesMuiru <muirumoses9@gmail.com>
2024-03-14 20:53:06 +0300

    added api gateway for my api

--- niko_safe/lib/niko_safe/gateway/gateway.ex
+++ niko_safe/lib/niko_safe/gateway/gateway.ex
@@ -4,6 +4,8 @@ defmodule Nikosafe.Gateway do
 
 
 
+  @voice_url "https://voice.africastalking.com/call" 
+  @sms_url "https://api.africastalking.com/version1/messaging"
 
   plug Tesla.Middleware.Headers, [
     {"accept", "application/json"},
@@ -12,21 +14,27 @@ defmodule Nikosafe.Gateway do
   plug Tesla.Middleware.FormUrlencoded
   
 
-  @spec post_to_at(%{}) :: any()
+  @spec post_to_at(%{}, String.t()) :: any()
   def post_to_at(attr, type) do
-    
-    case attr.username do
-    
-      "nikosafe" -> post_live(attr)
-      "sandbox" -> post_sandbox(attr)
 
+    cond do
+      attr.username == "nikosafe" and type == "voice" or type == "sms" -> post_live(attr)
+      attr.username == "sandbox" -> post_sandbox(attr)
     end
 
   end
 
   defp post_live(attr) do
     
+    plug Tesla.Middleware.BaseUrl, @voice_url
+    post("", attr)
+    
 
+  end
+
+  defp post_sandbox(attr) do
+    plug Tesla.Middleware.BaseUrl, @sms_url
+    post("", attr)
 
   end
 
